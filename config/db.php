342dd88d6743da49dd26490e5cf9dd9f287ee75c
lransau <magnatiwe@mail.ru>
2017-09-13 14:48:50 +0700

    init

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=nsau',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+];
